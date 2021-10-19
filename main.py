f7c3d26dcc1f1bfb6c46f70cd934d3b6425c27d0
frank <f23542@naver.com>
2021-10-20 04:31:51 +0900

    2

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-#    print_hi('PyCharm !')
+
+    #print_hi('PyCharm !')
 
     app = QApplication(sys.argv)
 
